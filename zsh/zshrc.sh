ad3c11c4433889b5fbe42773885b1da937a2dcc0
Juan Antonio Barragan <jbarrag3@jh.edu>
2023-05-27 20:09:37 -0400

    zsh and vim updates

--- zsh/zshrc.sh
+++ zsh/zshrc.sh
@@ -28,11 +28,26 @@ unsetopt BEEP
 
 # useful custom functions 
 source "$HOME/mydotfiles/zsh/zsh-functions.sh"
+
+#zsh_add_file $HOME/mydotfiles/zsh/zsh-vim-mode
 # Plugins
 zsh_add_plugin "zsh-users/zsh-autosuggestions"
 zsh_add_plugin "zsh-users/zsh-syntax-highlighting"
+
+function zvm_config() {
+  ZVM_LINE_INIT_MODE=$ZVM_MODE_INSERT
+  ZVM_VI_INSERT_ESCAPE_BINDKEY=jk
+  ZVM_VI_SURROUND_BINDKEY="classic"
+}
+
+zsh_add_plugin "jeffreytse/zsh-vi-mode"
+
 #Prompt
 export PS1="%K{blue}%n@%m%k %B%F{cyan}%(4~|...|)%3~%F{white} %# %b%f%k"
 #Aliases
 source ~/mydotfiles/aliases/aliases.sh
+
+source $HOME/mydotfiles/aliases/aliases.sh
+source $HOME/mydotfiles/nnn/nnnrc.sh
+source /usr/share/autojump/autojump.sh
 
