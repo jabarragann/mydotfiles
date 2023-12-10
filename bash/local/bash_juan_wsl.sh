6b3633590914e4d0471566ffe502bd12c461121a
Juan Antonio Barragan <jbarrag3@jh.edu>
2023-12-09 23:28:50 -0500

    Update local config

--- bash/local/bash_juan_wsl.sh
+++ bash/local/bash_juan_wsl.sh
@@ -2,19 +2,19 @@
 # Sym link to  ~/.bashrc.local with:  
 # ln -s ./mydotfiles/bash/local/bash_juan_wsl.sh ./.bashrc.local
 
-ros()
-{
-    source /opt/ros/noetic/setup.bash
-    export PATH=$PATH:/home/juan/research/ambf/bin/lin-x86_64
-}
+# ros()
+# {
+#     source /opt/ros/noetic/setup.bash
+#     export PATH=$PATH:/home/juan/research/ambf/bin/lin-x86_64
+# }
 
 #source /home/juan/terminal_config/nnnrc.sh
-source $HOME/mydotfiles/nnn/nnnrc.sh
+# source $HOME/mydotfiles/nnn/nnnrc.sh
 # source $HOME/mydotfiles/bash/bashrc.sh
 
 ### WSL specific config 
-alias cae='conda activate'
-alias cde='conda deactivate'
+# alias cae='conda activate'
+# alias cde='conda deactivate'
 alias files='explorer.exe .'
 
 # Intermediate programming
