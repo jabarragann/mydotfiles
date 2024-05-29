b041b87f0ad91dd83319d54bfde467f12002e88c
Juan Antonio Barragan <jbarrag3@jh.edu>
2024-05-29 16:39:49 -0400

    Add `$HOME/.local/bin` to PATH

--- bash/bashrc.sh
+++ bash/bashrc.sh
@@ -8,6 +8,9 @@ source /usr/share/autojump/autojump.sh
 #bash prompt
 source $HOME/mydotfiles/bash/bashcmdprompt.sh
 
+#add local bin folder to path
+PATH=$PATH:$HOME/.local/bin/
+
 #local configs
 if [ -f ~/.bashrc.local ]; then
     source ~/.bashrc.local
