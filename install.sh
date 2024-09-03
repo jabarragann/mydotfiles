ae052571aafa5f2b2925f669ec166f13c8671bc3
Juan Antonio Barragan <jbarrag3@jh.edu>
2024-09-03 12:14:02 -0400

    Update installation scripts
    - Fix nnn automatic installation
    - Vim plug automatic installation

--- install.sh
+++ install.sh
@@ -4,19 +4,6 @@ mydotfiles="$HOME/mydotfiles"
 programsdir="$HOME/programs/"
 mkdir -v -p $programsdir
 
-# Download nnn 
-echo -e "\n <-----Downloading nnn -----> \n"
-wget -O $programsdir/nnn-v5.0.tar.gz -nv https://github.com/jarun/nnn/releases/download/v5.0/nnn-v5.0.tar.gz
-tar xf $programsdir/nnn-v5.0.tar.gz -C $programsdir 
-
-# Download nerd fonts
-echo -e "\n <-----Downloading nerd fonts-----> \n"
-local_fonts_dir="$HOME/.local/share/fonts/nerdfonts/"
-mkdir -v -p $local_fonts_dir
-
-nerd_fonts_url=https://github.com/ryanoasis/nerd-fonts/releases/download/v3.0.2/Hack.zip
-wget -O $local_fonts_dir/Hack_v3.0.2.zip -nv  $nerd_fonts_url
-unzip $local_fonts_dir/Hack_v3.0.2.zip -d $local_fonts_dir/
 
 # Create symbolic links
 echo -e " \n <-----Creating symbolic links-----> \n"
@@ -28,13 +15,39 @@ ln -s $HOME/mydotfiles/aliases/aliases.sh $HOME/.aliases
 # Add juan configs to bashrc
 echo 'source $HOME/mydotfiles/bash/bashrc.sh' >> ~/.bashrc
 
+# INSTALL NERD FONTS
+#####################
+
+echo -e "\n <-----Downloading nerd fonts-----> \n"
+local_fonts_dir="$HOME/.local/share/fonts/nerdfonts/"
+mkdir -v -p $local_fonts_dir
+
+nerd_fonts_url=https://github.com/ryanoasis/nerd-fonts/releases/download/v3.0.2/Hack.zip
+wget -O $local_fonts_dir/Hack_v3.0.2.zip -nv  $nerd_fonts_url
+unzip $local_fonts_dir/Hack_v3.0.2.zip -d $local_fonts_dir/
+
 # Regenerate font cache
 fc-cache -f 
 echo | fc-list | grep "Hack"
 
+# NNN INSTALLATION
+##################
+
+echo -e "\n <-----Downloading nnn -----> \n"
+
+wget -O $programsdir/nnn-v5.0.tar.gz -nv https://github.com/jarun/nnn/releases/download/v5.0/nnn-v5.0.tar.gz
+tar xf $programsdir/nnn-v5.0.tar.gz -C $programsdir 
+
+LOCAL_BIN=$HOME/.local/bin
+if [ ! -d $LOCAL_BIN  ]; then
+    mkdir -p $LOCAL_BIN
+    echo "Creating local bin folder: $LOCAL_BIN"
+fi
+
+echo -e "\n <-----Compiling nnn -----> \n"
+
 # Compile and install nnn
-mkdir ~/.local/bin
-cd ~/$programsdir/nnn-5.0
+cd $programsdir/nnn-5.0
 make O_NERD=1
-cp ./nnn ~/.local/bin/
+cp ./nnn $LOCAL_BIN  
 
