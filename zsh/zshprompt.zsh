c2e2afc0caa45ec729dfa728ea7dc8f65bc34b78
Juan Antonio Barragan <jbarrag3@jh.edu>
2026-06-12 15:05:10 -0400

    Update zsh prompt

--- zsh/zshprompt.zsh
+++ zsh/zshprompt.zsh
@@ -8,6 +8,6 @@ zstyle ':vcs_info:*' enable git
 zstyle ':vcs_info:git:*' formats '(%b)%u%c'
 zstyle ':vcs_info:git:*' check-for-changes true
 zstyle ':vcs_info:git:*' unstagedstr '|+'
-zstyle ':vcs_info:git:*' stagedstr '|+'
+zstyle ':vcs_info:git:*' stagedstr '|*'
 
-PROMPT='%F{green}%B%n@%m%b%f:%F{blue}%B%~%b%f %F{red}${vcs_info_msg_0_}%f %# '
+PROMPT='%F{green}%B%n@%m%b%f:%F{blue}%B%~%b%f %F{red}${vcs_info_msg_0_}%f %F{cyan}${ROS_DISTRO:+(${ROS_DISTRO})}%f %# '
